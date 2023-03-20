2829f6502a7d237835d5503fdb9a29acb5c5cd8c
adams <stilwelladam8@gmail.com>
2023-03-19 22:28:24 -0400

    Second build. It is working, but the code is very sloppy. Will come back at some point.

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -1,4 +1,4 @@
-package hammurabi.src.main.java;
+package hamurabi.src.main.java;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -15,11 +15,8 @@ public class Hammurabi {
     private int citizensArrived, plagueDeaths, starvationDeaths;
     private int totalDeaths, highestPop, highestGrain, totalStarved, totalNewCitizens, totalGrownGrain, happinessTracker,
             totalRatEatGrain, totalPlagueDeaths;
-    private double ratFeast, grainsBeforeRats;
-    String ratAttack, plagueAttack;
-    String immigrationMessage;
-    String starvationMessage;
-    boolean isStarving, hasRats, hasPlague, hasImmigration;
+    private double ratFeast;
+    boolean isStarving = false;
     boolean isPlayable = true;
     boolean isBuyTime = false;
     boolean isSellTime = false;
@@ -40,19 +37,14 @@ public class Hammurabi {
         starvationDeaths = 0;
         currentYear = 1;
         happinessTracker = 0;
+        highestGrain = currentGrain;
+        highestPop = currentPop;
 
         String startingString = "\n\nThe year is 1780 B.C. The people of the great city Babylon have entrusted you, Hamurabi, as their ruler.\n" +
                 "Entrusted to you are the fields of grain, and their harvest.  The people sustain themselves by grain, but the grain is also a central " +
                 "point in our economy.\nYou will have to spend grain to make grain, but in doing so, our city will thrive. " +
                 "Our city has had known issues with both plague and rats. Be careful to watch our reserves.\n" +
                 "Never let our people go hungry. O great Hamurabi, we have put our trust in you. Do not let our people starve.";
-        starvationMessage = "Your rule has led to the population starving!! As a result " + starvationDeaths + " have died, leaving us with a total population of "+
-        currentPop + ".";
-        ratAttack = "\nYour grain stores were infested with rats. The rats ate " + ratFeast + "of the " + grainsBeforeRats +
-                " grains in storage. Leaving us with a total of " + currentGrain + "grains";
-        plagueAttack = "\nOur citizens were ravaged by the plague!! The plague killed a total of " + plagueDeaths + " citizens leaving us with a total of " +
-                currentPop + "citizens.";
-        immigrationMessage = "\nOur city is thriving!! Your success has inspired " +citizensArrived + " to migrate to our city! We now support a population of " +currentPop + ".";
         sb.append(startingString);
 
         return sb.toString();
@@ -66,25 +58,6 @@ public class Hammurabi {
                     "\nIn the previous year, " + starvationDeaths + " citizens starved.\n" + citizensArrived +" citizens immigrated to the city.";
             sb.append(currentStats);
             System.out.println(currentStats);
-            if(currentYear > 1){
-                if(isStarving){
-                    sb.append(starvationMessage);
-                    isStarving = false;
-                }
-                if(hasPlague){
-                    sb.append(plagueAttack);
-                    hasPlague = false;
-                }
-                if(hasRats){
-                    sb.append(ratAttack);
-                    hasRats = false;
-                }
-                if(hasImmigration){
-                    sb.append(immigrationMessage);
-                    hasImmigration = false;
-                }
-                System.out.println(sb);
-            }
             getPlayerInput();
             yearEnd();
             currentYear++;
@@ -93,10 +66,11 @@ public class Hammurabi {
             }
         }
         System.out.println(finalSummary());
+        System.exit(1);
     }
 
     public int getPlayerInput(){
-        currentGrain = buyLand();
+        buyLand();
         currentGrain -= feedPeople();
         currentGrain += (currentAcreValue * sellAcres());
         currentGrain -= (plantGrain()*2);
@@ -108,8 +82,23 @@ public class Hammurabi {
         sb.append("\n\nCurrently you have " + currentGrain + " bushels of grain. Each acre of land is valued at " + currentAcreValue +
                 ". How many acres of land would you like to buy.");
         System.out.println(sb);
-        int grainSpent = spendingGrain();
-        currentAcres += (grainSpent*currentAcreValue);
+        isBuyTime = true;
+        int acresBought;
+        while(isBuyTime){
+            try {
+                acresBought = in.nextInt();
+                if((acresBought*currentAcreValue) >currentGrain){
+                    System.out.println("Surely you jest. You do not have the grain to do that!");
+                }else{
+                    isBuyTime = false;
+                    currentGrain -= (acresBought * currentAcreValue);
+                    currentAcres += acresBought;
+                    return acresBought;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("You silly old fool, enter a viable number!");
+            }
+        }
         return currentGrain;
     }
 
@@ -137,21 +126,21 @@ public class Hammurabi {
 
     public int spendingGrain(){
         isBuyTime = true;
-        int grainToSpend;
+        int grainSpent;
         while(isBuyTime){
             try {
-                grainToSpend = in.nextInt();
-                if(grainToSpend>currentGrain){
+                grainSpent = in.nextInt();
+                if(grainSpent>currentGrain){
                     System.out.println("Surely you jest. You do not have the grain to do that!");
                 }else{
                     isBuyTime = false;
-                    return grainToSpend;
+                    return grainSpent;
                 }
             }catch(InputMismatchException e){
                 System.out.println("You silly old fool, enter a viable number!");
             }
         }
-        return 0;
+        return currentGrain;
     }
 
     public int sellAcres(){
@@ -182,7 +171,7 @@ public class Hammurabi {
         int maxAcresPlant = currentPop *10;
         sb.setLength(0);
         sb.append("\nCurrently we have " + currentGrain + " bushels of grain. The population is " + currentPop + " citizens. We have " + currentAcres + " acres of land.\n" +
-                "Each citizen can plant 10 acres of land, at the cost of 2 grains per acre.\nHow many acres would you like to plant?");
+                "Each citizen can plant 10 acres of land, at the cost of 2 bushels of grains per acre.\nHow many acres would you like to plant?");
         System.out.println(sb);
         int acresDesired = 0;
         boolean isPlantTime = true;
@@ -207,79 +196,81 @@ public class Hammurabi {
         return 0;
     }
 
-    public boolean plagueTime(){
+    public int plagueTime(){
         //15%
         if(rand.nextInt(100)< 15){
-            hasPlague = true;
             plagueDeaths = currentPop/2;
             currentPop /=2;
             totalPlagueDeaths+= plagueDeaths;
             totalDeaths += plagueDeaths;
-            System.out.println("Our city has been hit with a terrible plague, we have lost " + plagueDeaths + " citizens.");
+            System.out.println("\nOur city has been hit with a terrible plague, we have lost " + plagueDeaths + " citizens.");
         }
 
-        return hasPlague;
+        return plagueDeaths;
     }
 
-    public boolean ratTime(){
+    public int ratTime(){
         //40%
         if(rand.nextInt(100) < 40){
-            hasRats = true;
-            grainsBeforeRats = currentGrain;
             double percentDecrease = rand.nextInt(10)+21;
             percentDecrease /= 100;
             ratFeast = currentGrain * percentDecrease;
             totalRatEatGrain += ratFeast;
-            currentGrain -= ratFeast;
-            System.out.println("Our grain reserves have been infested with rats!! We have lost a total of " + ratFeast
+            currentGrain -= (int) ratFeast;
+            System.out.println("\nOur grain reserves have been infested with rats!! We have lost a total of " + (int) ratFeast
             + " bushels of grain! Where is that dang rat poison!?");
         }
-        return hasRats;
+        return currentGrain;
     }
 
     public int starvationTime(int grainsFed){
         int grainsNeeded = currentPop *20;
         int citizensDead = 0;
         double upRisingLimit = currentPop * 0.45;
+        //determine the number of deaths
         for(int i = grainsFed; i < grainsNeeded; i+=20){
             citizensDead++;
         }
-        System.out.println("Your city is starving!! As a result " + citizensDead + " citizens have died!");
+        //adjust total values of deaths
+        totalDeaths += citizensDead;
+        starvationDeaths += citizensDead;
+        totalStarved += starvationDeaths;
+        System.out.println("\nYour city is starving!! As a result " + citizensDead + " citizens have died!");
+        //if enough citizens died, cause uprising
         if(citizensDead > upRisingLimit){
             upRising();
         }
-        totalDeaths += citizensDead;
-        starvationDeaths += citizensDead;
         return citizensDead;
     }
 
-    public boolean immigrationTime(){
+    public int immigrationTime(){
+        //determine how many people immigrate to the city
         if(!isStarving){
             citizensArrived = (20 * currentAcres + currentGrain) / (100 * currentPop) + 1;
             totalNewCitizens += citizensArrived;
             currentPop += citizensArrived;
-
+            //adjust high score of population
             if(currentPop>highestPop){
                 highestPop = currentPop;
             }
         }
-        return hasImmigration;
+        return currentPop;
     }
 
-
     public int landCost(){
-        //17 to 23
+        //change the value of land to a random number between 17 and 23
         currentAcreValue = rand.nextInt(7)+17;
         return currentAcreValue;
     }
 
     public void upRising(){
-        //45%
+        //close out the game
         sb.setLength(0);
-        sb.append("We have lost majority of our population. You have disgraced this city. The citizens have formulated a plot against you." +
+        sb.append("We have lost majority of our population. You have disgraced this city. The citizens have formulated a plot against you.\n" +
                 "They demand your seat of power... and your head. \n\nGame Over.");
         System.out.println(sb);
         popUprose = true;
+        isPlayable = false;
         finalSummary();
     }
 
@@ -288,59 +279,66 @@ public class Hammurabi {
         int grownGrain = harvestRate * currentPlantedAcres;
         totalGrownGrain += grownGrain;
         currentGrain += grownGrain;
-
-        System.out.println("The harvest was");
-
+        //set high score of grain
         if(currentGrain>highestGrain){
             highestGrain = currentGrain;
         }
-
+        //display messages to the player depending on how much grain they harvested
         if(harvestRate == 1){
-            System.out.println("This year's harvest has been pitiful, we have lost more grain than we gained. Hopefully next year is better.");
+            System.out.println("\nThis year's harvest has been pitiful, we have lost more grain than we gained." +
+                    " We harvested " + grownGrain + " bushels of grain, bringing our current stores to " + currentGrain + " bushels of grain."
+                    + " Hopefully next year is better.");
         }else if(harvestRate == 2){
-            System.out.println("This year's harvest has been adequate, at least we did not lose any grain");
+            System.out.println("\nThis year's harvest has been adequate, at least we did not lose any grain. We harvested "
+                    + grownGrain + " bushels of grain, bringing our current stores to " + currentGrain +" bushels of grain.");
         }else if(harvestRate < 6){
-            System.out.println("This year's harvest has been plentiful, what a joy it is to see our crops thrive.");
+            System.out.println("\nThis year's harvest has been plentiful." + " We harvested " + grownGrain + " bushels of grain, bringing our current stores to "
+                    + currentGrain + " bushels of grain. What a joy it is to see our crops thrive.");
         }else{
-            System.out.println("This year's harvest has been overwhelmingly good. The grain is plentiful, and the people respect your leadership.");
+            System.out.println("\nThis year's harvest has been overwhelmingly good. We harvested " + grownGrain
+                    + " bushels of grain, bringing our current stores to " + currentGrain +
+                    " bushels of grain. The grain is plentiful, and the people respect your leadership.");
         }
         return currentGrain;
     }
 
-    public String yearEnd(){
+    public void yearEnd(){
         plagueTime();
         ratTime();
         immigrationTime();
         landCost();
         harvestTime();
-        return sb.toString();
     }
 
     public String finalSummary(){
         sb.setLength(0);
-        sb.append("\nAt the end of your rule you had a total death count of " + totalDeaths + ".\nYou grew a total of " + totalGrownGrain
-                + ".\nYou enticed " + totalNewCitizens + " to join your city.\nThe rats ate a total of " +totalRatEatGrain
-                + ".\nThe plague claimed a total of " + totalPlagueDeaths +" deaths.\nYou starved a total of "+ totalStarved +" citizens."
-                +"\nYour highest population was " + highestPop + "\nThe highest grain amount of grain you stored was " +highestGrain +".");
+        sb.append("\nAt the end of your rule you had a total death count of " + totalDeaths + ".\nYou grew a total of " + totalGrownGrain + " bushels of grain."
+                + "\nYou enticed " + totalNewCitizens + " new citizens to join your city.\nThe rats ate a total of " +totalRatEatGrain + " bushels of grain."
+                + "\nThe plague claimed a total of " + totalPlagueDeaths +" people.\nYou starved a total of "+ totalStarved +" citizens."
+                +"\n\nYour highest population was " + highestPop + " citizens.\nThe highest amount of grain you had stored was " +highestGrain +".");
         if(popUprose){
-            sb.append("\nFinal rating: \n\nThe city is most likely better off without you. Maybe the rats and the plagues are to blame, but the people do not care." +
-                    " You chose to lead, so you have to deal the the consequences. It doesn't seem as though the angry mob will listen to reason though.");
+            sb.append("\n\nFinal rating:\n\nThe city is most likely better off without you. Maybe the rats and the plagues are to blame, but the people do not care.\n" +
+                    "You chose to lead, so you have to deal the the consequences. It doesn't seem as though the angry mob will listen to reason though.");
+            System.out.println(sb);
+            System.exit(1);
         }
         if(happinessTracker == 0){
-            sb.append("\nFinal rating: \n\nYou ruled the people hard. You may have given your populace enough food to live, but that was all you did. " +
+            sb.append("\n\nFinal rating:\n\nYou ruled the people hard. You may have given your populace enough food to live, but that was all you did. " +
                     "You ruled all ten years, but the people will not remember you for your kindness. None will sing songs of you in the future.");
         }else if(happinessTracker > 0 && happinessTracker <= 3){
-            sb.append("\nFinal rating: \n\nThe long ten years have come to a close and the people are grateful for the small amount of kindness you have shown them." +
+            sb.append("\n\nFinal rating:\n\nThe long ten years have come to a close and the people are grateful for the small amount of kindness you have shown them." +
             "You did not have to share more with the people of Babylon, but you did. It wasn't much, but the people noticed and praise you for it.");
         }else if(happinessTracker >=4 && happinessTracker<=7){
-            //not done
-            sb.append("\nFinal rating: \n\nThe people noticed your generosity, however small, and praise you generously for your rule.");
+            sb.append("\n\nFinal rating:\n\nThe people noticed your generosity, however small, and praise you generously for your rule." +
+                    " So much so, they have decided to re-elect you immediately.");
         }else if(happinessTracker>=8 && happinessTracker<=9){
-            //not done
-            sb.append("");
+            sb.append("\n\nFinal rating:\n\nYou have kept the city well fed, so well fed that they have named a holiday after you." +
+                    " The Hammurabi Hammy Time. A full week off of work to celebrate the love they have for you.");
         }else if(happinessTracker == 10){
             //not done
-            sb.append("");
+            sb.append("\n\nFinal rating:\n\nThe people of Babylon have determined you as God reincarnated." +
+                    " As a result they have erected statues of your likeness throughout the city. " +
+                    "There are constant parades in your honor, and your every whim and desire is always met by a smattering of servants.");
         }
         return sb.toString();
     }
